e113a3d5384ece2ef6ac36314e4282bfa0da8d2b
Ahmed Shehab <ahmed4ehab@yahoo.com>
2024-11-07 15:11:58 +0200

    feat: alter content and sender max_length

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Message(models.Model):
-    sender = models.CharField(max_length=100)
-    content = models.TextField()
+    sender = models.CharField(max_length=50)
+    content = models.TextField(max_length=1024)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
